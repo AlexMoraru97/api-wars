5e78572b40c10b80d88a74b4404d9233d51af1a9
alexmoraru97 <alex.moraru97@gmail.com>
2021-06-28 17:04:20 +0300

    implemented page switching

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,7 +1,7 @@
-window.onload = function(ev){
+window.onload = (ev) => {
     let source = "https://swapi.dev/api/planets/";
 
-    function updatePlanetList() {
+    const updatePlanetList = () =>{
         const planetList = document.getElementById('planets-table-list');
         planetList.innerHTML = ''
 
@@ -9,16 +9,42 @@ window.onload = function(ev){
             data.results.forEach(result => {
                 const row = document.createElement('tr');
                 planetList.appendChild(row);
-                row.insertAdjacentHTML("afterbegin",
-                    `<td>${result.name}</td>
-                          <td>${result.diameter !== 'unknown'? result.diameter+ ' km' : 'unknown' }</td>
-                          <td>${result.climate}</td>
-                          <td>${result.terrain}</td>
-                          <td>${result.surface_water !== 'unknown' ? result.surface_water + '%' : 'unknown'}</td>
-                          <td>${result.population !== 'unknown' ? result.population+ ' people' : 'unknown'}</td>
-                          `);
+                row.innerHTML =
+                 `<td>${result.name}</td>
+                  <td>${result.diameter !== 'unknown'? new Intl.NumberFormat('ro-RO').format(result.diameter)+ ' km' : 'unknown' }</td>
+                  <td>${result.climate}</td>
+                  <td>${result.terrain}</td>
+                  <td>${result.surface_water !== 'unknown' ? result.surface_water + '%' : 'unknown'}</td>
+                  <td>${result.population !== 'unknown' ? new Intl.NumberFormat('ro-RO').format(result.population)+ ' people' : 'unknown'}</td>
+                  <td>
+                    <button type="button" class="${result.residents.length === 0 ? "btn btn-outline-light disabled" : "btn btn-outline-warning"}">
+                            ${result.residents.length === 0 ? 'No known residents' : result.residents.length + ' resident(s)'}
+                    </button>
+                  </td>`;
             })
         })
     }
+
+    const choosePage = () => {
+        const buttons = document.querySelectorAll('#btn-next, #btn-previous');
+        for (let button of buttons) {
+            button.addEventListener("click", (event) => {
+                if (!event.detail || event.detail === 1) {
+                    fetch(source).then((response) => response.json()).then((data) => {
+                        if (data.next && (button.id === 'btn-next')) {
+                            source = data.next;
+                            updatePlanetList();
+                        }
+                        if (data.previous && (button.id === 'btn-previous')) {
+                            source = data.previous;
+                            updatePlanetList();
+                        }
+                    })
+                }
+            })
+        }
+    };
+
     updatePlanetList()
+    choosePage()
 }
